e27cb4d31ddc324d91d249ea1a53dac457a1a9db
Steve Robillard <steverobillard@gmail.com>
2020-03-26 20:51:26 -0400

    add local changes

--- compromised_password_check.py
+++ compromised_password_check.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env bash
 
 # todo add attributions
-# add explicit exit code
-# document including notes on k-anon 
+# document including notes on k-anon
 # add support for -h flag
+# add longer text abut changing password wherever it is used
+# add option to show password (hide by default)
+# move usage to a function so we can call it with -h
 
 # Attribution ====================================================================================
+# color from reddit
 # https://linuxacademy.com/blog/security/proactively-identifying-compromised-passwords-roadmap-to-securing-your-infrastructure/
-# Packt Mastering Linux Security Book and insert original authorsip 
+# Packt Mastering Linux Security Book and insert original authorsip
 
 RED='\033[1;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 NC='\033[0m'
+CHECKMARK='\xE2\x9C\x94'
+CROSS='\xE2\x9D\x8C'
 
 candidate_password=$1
 
-[ $# -eq 0 ] && { echo -e "\nUsage: compromisedpassword 'password'\n"; exit 1; }
+[ $# -eq 0 ] && { echo -e "\nUsage: ./compromisedpassword 'password'\n"; exit 1; }
 
-printf "\nChecking candidate password $candidate_password\n\n"
+printf "\nChecking candidate password %s\n" "$candidate_password"
 
-full_hash=$(echo -n $candidate_password | sha1sum | awk '{print substr($1, 0, 32)}')
-prefix=$(echo $full_hash | awk '{print substr($1, 0, 6)}')
-suffix=$(echo $full_hash | awk '{print substr($1, 6, 26)}')
-if curl https://api.pwnedpasswords.com/range/$prefix -H "Add-Padding: true"  | grep -i $suffix;
-  then 
-    printf "\n${RED}Candidate password $candidate_password is compromised${NC}\n\n";
-  else 
-    printf "\n${GREEN}Candidate password $candidate_password is OK for use${NC}\n\n";
+full_hash=$(echo -n "$candidate_password" | sha1sum | awk '{print substr($1, 0, 32)}')
+prefix=$(echo "$full_hash" | awk '{print substr($1, 0, 6)}')
+suffix=$(echo "$full_hash" | awk '{print substr($1, 6, 26)}')
+if curl https://api.pwnedpasswords.com/range/"$prefix" -s -H "Add-Padding: true"  | grep -q -i "$suffix";
+then
+    echo -e "\n${RED}${CROSS} Candidate password $candidate_password is compromised ${NC}\n\n"
+    echo -e "You should immediately change this password on any other site you amay be using it. More information can be found here (insert link to HIBP\n\n"
+else
+    echo -e "\n${GREEN}${CHECKMARK} Candidate password $candidate_password is safe for use${NC}\n\n"
 fi
 exit 0;
